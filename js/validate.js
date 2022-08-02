ed69f79e0ca01e673412b632895a1d6c635c94ca
Mattia Brambilla <30724389+mattbrambilla@users.noreply.github.com>
2022-08-02 16:26:50 +0200

    Edited form for mail

--- js/validate.js
+++ js/validate.js
@@ -1,6 +1,8 @@
 function validate (){
     var name = document.getElementById(name);
     var surname = document.getElementById(surname);
-    var email = document.getElementById(mail);
+    var mail = document.getElementById(mail);
     var description = document.getElementById(description);
+
+
 }
