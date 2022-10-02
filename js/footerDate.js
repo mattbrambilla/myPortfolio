522227fddf86c55cdc7d62eaf14408ff1224b367
Mattia Brambilla <30724389+mattbrambilla@users.noreply.github.com>
2022-10-02 14:32:14 +0200

    Insert data script into a separate file + fixed contact section

---
+++ js/footerDate.js
@@ -0,0 +1,2 @@
+  // Script per ottenere l'anno corrente e stamparlo nel footer
+  document.getElementById("year").innerHTML = new Date().getFullYear();
--- js/validate.js
+++
@@ -1,8 +0,0 @@
-function validate (){
-    var name = document.getElementById(name);
-    var surname = document.getElementById(surname);
-    var mail = document.getElementById(mail);
-    var description = document.getElementById(description);
-
-
-}
